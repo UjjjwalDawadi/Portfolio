09d853fcbd3463b0f77942923c5dd9485a6378e7
Ujjwal <ujjwaldawadi25@gmail.com>
2023-09-15 23:19:14 +0545

    Adding extra js

--- script.js
+++ script.js
@@ -9,6 +9,26 @@ function toggleVerticalMenu() {
 
 $(document).ready(function () {
     $("#menu-icon").click(function () {
-        $("#vertical-menu").toggle(); // Toggle the visibility of the vertical menu
+        $("#vertical-menu").toggle();
     });
 });
+
+//  Script for Scroll to top button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
