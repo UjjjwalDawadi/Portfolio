f996ac467585916a35fd6ced85684999872c1cbe
Ujjwal <ujjwaldawadi25@gmail.com>
2023-09-17 22:54:38 +0545

    FInal edits complete

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ function toggleVerticalMenu() {
     }
 }
 
-$(document).ready(function () {
-    $("#menu-icon").click(function () {
-        $("#vertical-menu").toggle();
-    });
-});
 
-//  Script for Scroll to top button
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
+
 var mybutton = document.getElementById("myBtn");
+
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      mybutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+      mybutton.style.display = "none";
   }
 }
 
@@ -30,5 +30,26 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.header');
+  const firstSection = document.querySelector('#home');
+  
+  if (window.scrollY >= firstSection.offsetHeight) {
+      navbar.classList.add('fixed-header');
+  } else {
+      navbar.classList.remove('fixed-header');
+  }
+});
+
+$(document).ready(function () {
+    $("#menu-icon").click(function () {
+        $("#vertical-menu").toggle();
+    });
+});
+
+
+
+
 
 
